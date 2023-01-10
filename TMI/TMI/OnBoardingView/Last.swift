0c2d8cd60c62513b197c62a4a93a82f24dcbe5e8
김응관 <kwhn9486@naver.com>
2023-01-10 21:23:31 +0900

    Add : 필요한 View 파일들 추가 #4

---
+++ TMI/TMI/OnBoardingView/Last.swift
@@ -0,0 +1,20 @@
+//
+//  Last.swift
+//  TMI
+//
+//  Created by 김응관 on 2023/01/10.
+//
+
+import SwiftUI
+
+struct Last: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Last_Previews: PreviewProvider {
+    static var previews: some View {
+        Last()
+    }
+}
