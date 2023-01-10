4c4529771fa0f4998540c54cc864302cdb734a5a
김응관 <kwhn9486@naver.com>
2023-01-10 21:22:05 +0900

    Add : 필요한 View 파일들 추가

---
+++ TMI/TMI/OnBoardingView/Third.swift
@@ -0,0 +1,20 @@
+//
+//  Third.swift
+//  TMI
+//
+//  Created by 김응관 on 2023/01/10.
+//
+
+import SwiftUI
+
+struct Third: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Third_Previews: PreviewProvider {
+    static var previews: some View {
+        Third()
+    }
+}
