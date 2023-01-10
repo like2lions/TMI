8e3930e59ef66f2f31db29fbde7111b44896ca67
김응관 <kwhn9486@naver.com>
2023-01-10 20:44:17 +0900

    Add : OnBoardingView 폴더 및 View 파일 생성 #4

---
+++ TMI/TMI/OnBoardingView/OnBoarding.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoarding.swift
+//  TMI
+//
+//  Created by 김응관 on 2023/01/10.
+//
+
+import SwiftUI
+
+struct OnBoarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoarding_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoarding()
+    }
+}
