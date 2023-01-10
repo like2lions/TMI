72654a3f49162c1ec9fd6880cb0a98879860d97f
ehvkddl <ehvkddl@naver.com>
2023-01-10 22:58:24 +0900

    feat : Terminal Bar View 생성 #3

--- TMI/TMI/Model/HomeView.swift
+++ TMI/TMI/Model/HomeView.swift
@@ -9,10 +9,9 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-                
-            terminalBar()
-
+        VStack(spacing: 1) {
+            terminalBar(user: "Chap", path: "~")
+            terminalBar(user: "Chap", path: "~/Desktop")
         }
     }
 }
@@ -24,29 +23,68 @@ struct HomeView_Previews: PreviewProvider {
 }
 
 struct terminalBar: View {
+    let user: String
+    let path: String
+    
+    let firstColor: Color = .black
+    let secondColor: Color = .yellow
+    let fontColor: Color = .white
+    
     var body: some View {
-        HStack {
-            Text("안녕하세요")
-                .frame(height: 20.5)
-                .padding(.horizontal,10)
-                .foregroundColor(.white)
-                .background(.black)
-            Image("bar1")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(.leading, -10)
-            Text("오늘 해야")
-                .frame(width: .infinity ,height: 20.5)
-                .foregroundColor(.white)
-                .padding(.horizontal,10)
-                .background(Color("barcolor"))
-                .padding(.leading, -8)
-            Rectangle()
-                .rotationEffect(.init(degrees: 45))
-                .frame(width: 14.6, height: 14.6)
-                .foregroundColor(Color("barcolor"))
-                .padding(.leading, -15)
+        VStack {
+            HStack(spacing: 0) {
+                Text("\(user)")
+                    .bold()
+                    .padding(.horizontal, 10)
+                    .frame(height: 30)
+                    .foregroundColor(fontColor)
+                    .background(firstColor)
+                ZStack {
+                    MyShape()
+                        .foregroundColor(firstColor)
+                    MyShape2()
+                        .foregroundColor(secondColor)
+                }
+                .frame(width: 20, height: 30)
+                Text("\(path)")
+                    .bold()
+                    .padding(.horizontal, 10)
+                    .frame(height: 30)
+                    .foregroundColor(fontColor)
+                    .background(secondColor)
+                MyShape()
+                    .foregroundColor(secondColor)
+                    .frame(width: 20, height: 30)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
-        .frame(width: 200, height: 20.5)
+    }
+}
+
+struct MyShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+//        path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.maxY),
+//            control: CGPoint(x: rect.midX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: (rect.minY + rect.maxY) / 2))
+        path.closeSubpath()
+        return path
+    }
+}
+
+struct MyShape2: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.maxX, y: (rect.minY + rect.maxY) / 2))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.closeSubpath()
+        return path
     }
 }
