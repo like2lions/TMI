17151604317bb10eb30ec0ae190c21d165f0ad08
ehvkddl <ehvkddl@naver.com>
2023-01-12 20:59:20 +0900

    feat : History structure, store 생성 #11

---
+++ TMI/TMI/Model/History.swift
@@ -0,0 +1,14 @@
+//
+//  History.swift
+//  TMI
+//
+//  Created by do hee kim on 2023/01/12.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    var id = UUID().uuidString
+    var command: String
+    var result: String
+}
---
+++ TMI/TMI/ViewModel/HistoryStore.swift
@@ -0,0 +1,12 @@
+//
+//  HistoryStore.swift
+//  TMI
+//
+//  Created by do hee kim on 2023/01/12.
+//
+
+import Foundation
+
+class HistoryStore: ObservableObject {
+    @Published var histories: [History] = []
+}
