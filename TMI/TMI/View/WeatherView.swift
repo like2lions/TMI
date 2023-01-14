5bbfcf58b2d5c395d5ec29160de7e4ad9647d70c
김응관 <kwhn9486@naver.com>
2023-01-15 03:28:34 +0900

    docs : WeatherView 추가 #18

---
+++ TMI/TMI/View/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  TMI
+//
+//  Created by 김응관 on 2023/01/15.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
