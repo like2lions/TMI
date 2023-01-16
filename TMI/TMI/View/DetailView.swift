813bbfa9fc43e0e96dafb981581d8be0296f95d1
ehvkddl <ehvkddl@naver.com>
2023-01-17 01:23:50 +0900

    feat : TextEditor Height에 따라 lineNumber 띄워주기 #22

--- TMI/TMI/View/DetailView.swift
+++ TMI/TMI/View/DetailView.swift
@@ -13,6 +13,7 @@ struct DetailView: View {
     @State private var text: String = ""
     @State var textArray: [String] = []
     @State var textEditorHeight : CGFloat = 0 // TextEditor의 높이를 구하기 위해 사용하는 변수
+    @State var lineIndex: Int = 0 // 입력된 line 수 보여주기 위한 변수
     @FocusState var focusField: Field?
     @ObservedObject var historyStore: HistoryStore
     
@@ -20,9 +21,19 @@ struct DetailView: View {
         
         VStack {
             HStack(spacing: 0) {
-                Rectangle()
-                    .frame(width: 30, height: textEditorHeight)
-                TextEditorView(text: $text, textEditorHeight: $textEditorHeight)
+                // lineNumber Rectangle
+                VStack(spacing: 0) {
+                    ForEach(0...lineIndex, id: \.self) { num in
+                        Rectangle()
+                            .frame(width: 35, height: 22)
+                            .overlay {
+                                Text("\(num + 1)")
+                                    .foregroundColor(.white)
+                            }
+                    }
+                }
+                // textEditor
+                TextEditorView(text: $text, textEditorHeight: $textEditorHeight, lineIndex: $lineIndex)
             }
             Spacer()
             HStack {
--- TMI/TMI/View/TextEditorView.swift
+++ TMI/TMI/View/TextEditorView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TextEditorView: View {
     @Binding var text: String
     @Binding var textEditorHeight: CGFloat // TextEditor의 높이를 구하기 위해 사용하는 변수
+    @Binding var lineIndex: Int // 입력된 line 수 보여주기 위한 변수
     
     var body: some View {
         ZStack(alignment: .leading) {
@@ -26,8 +27,21 @@ struct TextEditorView: View {
                 .frame(height: max(40,textEditorHeight))
                 .cornerRadius(10.0)
         }
-        .onPreferenceChange(ViewHeightKey.self) { newValue in
-            textEditorHeight = newValue
+        .onPreferenceChange(ViewHeightKey.self) { newHeight in
+            /// newHeight가 전보다 줄어들면 lineIndex - 1을 해야하고
+            /// 전보다 늘어나면 lineIndex  + 1을 해줘야한다
+            /// 첫번째 줄의 경우 아무것도 입력이 안되었을 때는 30인데 뭐 하나라도 입력되면 36.3333으로 높이가 늘어나버려서 첫번째 줄임에도 lineIndex가 2로 나와버리는 문제발생
+            /// 그래서 높이가 40 이하일 경우 lineIndex를 0으로 고정하였다
+            // TODO: - 다른 Device에서 첫번째 줄의 높이가 어떻게 나오는지 확인해보기
+            if newHeight < 40 {
+                lineIndex = 0
+            } else if textEditorHeight < newHeight {
+                lineIndex += 1
+            } else if textEditorHeight > newHeight {
+                lineIndex -= 1
+            }
+            
+            textEditorHeight = newHeight
         }
     }
 }
@@ -41,6 +55,6 @@ struct ViewHeightKey: PreferenceKey {
 
 struct TextEditorView_Previews: PreviewProvider {
     static var previews: some View {
-        TextEditorView(text: .constant(""), textEditorHeight: .constant(0))
+        TextEditorView(text: .constant(""), textEditorHeight: .constant(0), lineIndex: .constant(0))
     }
 }
